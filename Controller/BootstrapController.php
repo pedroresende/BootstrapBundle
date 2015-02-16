39d3702c65950d03f8fdfc9991e9d89a982fd1f1
Pedro Resende <pedroresende@mail.resende.biz>
2015-02-16 05:49:08 +0000

    Initial Commit

---
+++ Controller/BootstrapController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BootstrapBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
+
+class BootstrapController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BootstrapBundle::layout.html.twig');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+### Bootstrap Bundle
+
